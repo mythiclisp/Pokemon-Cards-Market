344b375769155702bd4f024d62ca7416bd784d3e
Kalan Jarvis-Loewen <Kalan@Robyns-MBP.vc.shawcable.net>
2021-12-13 01:55:02 -0800

    Changed JSX to TSX

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,6 +1,6 @@
 import styles from '../css/Layout.module.css'
-import Nav from '../components/Nav'
-import SignInModal from './Modals/modals'
+import Nav from './Nav'
+import SignInModal from './Modals/modals.jsx'
 
 const Layout = ({children}) => {
     return (
