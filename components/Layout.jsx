8181af91802685c3b628453a0a29953eeabfcd75
Kalan Jarvis-Loewen <theonlybaconsandwich@gmail.com>
2022-02-17 01:35:43 -0800

    Created minimal search filter

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,21 +1,29 @@
 import styles from '../css/Layout.module.css'
 import Nav from './Nav'
 import Modals from './Modals/modals.jsx'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import loadAnimation from '../Scripts/Animations/main'
-import { useRouter } from 'next/router';
+import SearchBar from './SearchBar'
 import BreadCrumbs from './breadCrumbs'
 import Footer from './Footer'
+import { db } from '../Scripts/firebaseconfig'
 
 const Layout = ({children}) => {
+    
+    let [posts, setPosts] = useState(null)
 
-    useEffect(() => {
+    async function getPosts() {
 
-    })
+        const posts = await db.collection("Posts").get()
+        return Promise.all(posts.docs.map(post => post))
+    }
+
+    getPosts().then(setPosts)
 
     return (
         <React.Fragment>
             <Nav/>
+            <SearchBar placeholder='enter card' data={posts}/>
             <Modals />
             <canvas id="bg" className={styles.animation_canvas}></canvas>
             <div className={styles.container}>
